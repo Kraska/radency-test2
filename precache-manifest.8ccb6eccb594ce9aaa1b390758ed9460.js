e31ef880fd2665a9a7c4b350b7af7bb829b11776
Savelieva Katerina <e.savelieva@owox.com>
2022-04-20 16:37:41 +0300

    Updates

---
+++ precache-manifest.8ccb6eccb594ce9aaa1b390758ed9460.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "3a45c4abe2303021b293",
+    "url": "/radency-test2/static/css/main.04f3b4fe.chunk.css"
+  },
+  {
+    "revision": "3a45c4abe2303021b293",
+    "url": "/radency-test2/static/js/main.8dbd4e06.chunk.js"
+  },
+  {
+    "revision": "943f5d7a3d466ed8af34",
+    "url": "/radency-test2/static/js/runtime~main.407202da.js"
+  },
+  {
+    "revision": "ede885a4f58b2b1e42d4",
+    "url": "/radency-test2/static/css/2.d26ef3f5.chunk.css"
+  },
+  {
+    "revision": "ede885a4f58b2b1e42d4",
+    "url": "/radency-test2/static/js/2.cbff968d.chunk.js"
+  },
+  {
+    "revision": "bdee2a94363933c9465b",
+    "url": "/radency-test2/static/js/3.859f812d.chunk.js"
+  },
+  {
+    "revision": "1ed478a6b265d4b4f5c26bb063203588",
+    "url": "/radency-test2/static/media/bootstrap-icons.1ed478a6.woff2"
+  },
+  {
+    "revision": "e4cc83ddcec34db65724eb56c5230b4c",
+    "url": "/radency-test2/static/media/bootstrap-icons.e4cc83dd.woff"
+  },
+  {
+    "revision": "6e4c98d3e6c6cedba7bc65eb177abf0c",
+    "url": "/radency-test2/index.html"
+  }
+];
